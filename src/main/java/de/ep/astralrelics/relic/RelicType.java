f5e18bd896eb8411cd1f1324eb42def818286141
catgod <paulhoerdt07@gmail.com>
2026-08-04 22:52:08 +0200

    Added other Relicts/Shards/Cores

--- src/main/java/de/ep/astralrelics/relic/RelicType.java
+++ src/main/java/de/ep/astralrelics/relic/RelicType.java
@@ -3,6 +3,14 @@ package de.ep.astralrelics.relic;
 public enum RelicType {
     ECHO_CORE,
     GALE_CORE,
+    Chrono_Core,
+    Gravity_Shard,
+    Frost_Relikt,
     PHOENIX_CORE,
-    ABYSS_CORE
+    ABYSS_CORE,
+    Shadow_Core,
+    Berseker_Core,
+    Mirror_Shard,
+    Nature_Core,
+    Magnet_Core
 }
