bc7ff4bf818f0c09a1c9bc588cd5ef212300be83
eliaswasem <ewasem@outlook.de>
2026-08-09 21:07:23 +0200

    fix: being able to put cores in hopper and with it in ChestMinecart, HopperMinecart and ChestBoat

---
+++ src/main/java/de/ep/astralcores/mixin/HopperBlockEntityMixin.java
@@ -0,0 +1,32 @@
+package de.ep.astralcores.mixin;
+
+import de.ep.astralcores.core.CoreFactory;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HopperBlockEntity.class)
+public class HopperBlockEntityMixin {
+
+    @Inject(
+            method = "addItem(Lnet/minecraft/world/Container;Lnet/minecraft/world/entity/item/ItemEntity;)Z",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void blockCoreItem(
+            Container container,
+            ItemEntity entity,
+            CallbackInfoReturnable<Boolean> cir
+    ) {
+        ItemStack stack = entity.getItem();
+
+        if (CoreFactory.getCoreFromItem(stack).isPresent()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
