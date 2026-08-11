218293c27c25e4eee3e79d973b3f8e5853f2dba9
catgod <paulhoerdt07@gmail.com>
2026-08-11 17:25:29 +0200

    Levitahn Core Passive ability

--- src/main/java/de/ep/astralcores/core/cores/LeviathanCore.java
+++ src/main/java/de/ep/astralcores/core/cores/LeviathanCore.java
@@ -2,7 +2,9 @@ package de.ep.astralcores.core.cores;
 
 import de.ep.astralcores.core.Core;
 import de.ep.astralcores.core.CoreType;
+import de.ep.astralcores.util.Effects;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Items;
 
 import java.util.List;
@@ -28,7 +30,18 @@ public class LeviathanCore extends Core {
 
     @Override
     public void applyPassive(ServerPlayer player) {
-
+        if (player.isInWater()) {
+            Effects.applyEffect(player, MobEffects.DOLPHINS_GRACE, 20, 2, false, false, false);
+            Effects.applyEffect(player, MobEffects.WATER_BREATHING, 20, 2, false, false, false);
+            Effects.applyEffect(player, MobEffects.CONDUIT_POWER, 20, 2, false, false, false);
+        }
+        if (player.isInWaterOrRain()) {
+            Effects.applyEffect(player, MobEffects.STRENGTH,20, 1, false, false, false);
+            Effects.applyEffect(player, MobEffects.SPEED,20, 1, false, false, false);
+            Effects.applyEffect(player, MobEffects.REGENERATION,20, 1, false, false, false);
+            Effects.applyEffect(player, MobEffects.HASTE,20, 1, false, false, false);
+            Effects.applyEffect(player, MobEffects.SATURATION,20, 1, false, false, false);
+        }
     }
 
     @Override
