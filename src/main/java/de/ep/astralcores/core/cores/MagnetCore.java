341b428a666e2bb58b6d26c7684237c0a59c50ec
catgod <paulhoerdt07@gmail.com>
2026-08-11 20:22:40 +0200

    Passive MagnetCore

--- src/main/java/de/ep/astralcores/core/cores/MagnetCore.java
+++ src/main/java/de/ep/astralcores/core/cores/MagnetCore.java
@@ -2,7 +2,9 @@ package de.ep.astralcores.core.cores;
 
 import de.ep.astralcores.core.Core;
 import de.ep.astralcores.core.CoreType;
+import de.ep.astralcores.util.Effects;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Items;
 
 import java.util.List;
@@ -27,12 +29,22 @@ public class MagnetCore extends Core {
     }
 
     @Override
-    public void applyPassive(ServerPlayer player) {
-        
+    public void applyPassive(ServerPlayer victim) {
+    if (Crit(victim)) {
+        Effects.applyEffect(victim, MobEffects.MINING_FATIGUE, 10, 255, false, false, false);
+        Effects.applyEffect(victim, MobEffects.WEAKNESS, 10, 255, false, false, false);
+        }
     }
 
     @Override
     public void activate(ServerPlayer player) {
+
+    }
+    private boolean Crit(ServerPlayer player) {
+        return !player.onGround()
+                && !player.hasEffect(MobEffects.BLINDNESS)
+                && !player.isSwimming()
+                && player.isFallFlying();
 
     }
 }
