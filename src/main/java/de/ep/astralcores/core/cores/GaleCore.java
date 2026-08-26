acae6f30f9b643ca3e69e2f0503f94f473d92084
eliaswasem <ewasem@outlook.de>
2026-08-26 23:07:13 +0100

    chore: add Zed project tasks

--- src/main/java/de/ep/astralcores/core/cores/GaleCore.java
+++ src/main/java/de/ep/astralcores/core/cores/GaleCore.java
@@ -1,21 +1,15 @@
 package de.ep.astralcores.core.cores;
 
+import java.util.List;
+
 import de.ep.astralcores.core.Core;
 import de.ep.astralcores.core.CoreType;
 import de.ep.astralcores.core.cores.logic.GaleCoreLogic;
-import de.ep.astralcores.util.TickTimer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Items;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 public class GaleCore extends Core {
 
-    // Stores the explosion timer for each player currently using Sonic Dash.
-    private static final Map<UUID, TickTimer> explosionTimers = new HashMap<>();
 
     public GaleCore() {
         super(
