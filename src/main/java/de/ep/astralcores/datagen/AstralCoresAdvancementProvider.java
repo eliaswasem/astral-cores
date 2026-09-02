179b4de768521472608eb31a9ef373d2c0de2a0f
eliaswasem <ewasem@outlook.de>
2026-09-02 19:08:32 +0100

    feat(Advancement): added VNature code advancement

--- src/main/java/de/ep/astralcores/datagen/AstralCoresAdvancementProvider.java
+++ src/main/java/de/ep/astralcores/datagen/AstralCoresAdvancementProvider.java
@@ -5,24 +5,27 @@ import de.ep.astralcores.advancement.criterion.CriterionRegistry;
 import de.ep.astralcores.advancement.criterion.criterions.VoidSurvivalCriterion;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
-import net.minecraft.advancements.Advancement;
-import net.minecraft.advancements.AdvancementHolder;
-import net.minecraft.advancements.AdvancementRewards;
-import net.minecraft.advancements.AdvancementType;
-import net.minecraft.advancements.predicates.LocationPredicate;
-import net.minecraft.advancements.predicates.MinMaxBounds;
+import net.minecraft.advancements.*;
+import net.minecraft.advancements.predicates.*;
 import net.minecraft.advancements.predicates.entity.EntityPredicate;
-import net.minecraft.advancements.triggers.CriteriaTriggers;
+import net.minecraft.advancements.triggers.InventoryChangeTrigger;
 import net.minecraft.advancements.triggers.PlayerTrigger;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.component.predicates.DataComponentPredicate;
+import net.minecraft.core.component.predicates.DataComponentPredicates;
+import net.minecraft.core.component.predicates.EnchantmentsPredicate;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.entity.EntityTypes;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.block.Blocks;
 
-
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
@@ -62,7 +65,9 @@ public class AstralCoresAdvancementProvider extends FabricAdvancementProvider {
                                         .vehicle(
                                                 EntityPredicate.Builder.entity()
                                                         .of(
-                                                                lookup.lookupOrThrow(Registries.ENTITY_TYPE),
+                                                                lookup.lookupOrThrow(
+                                                                        Registries.ENTITY_TYPE
+                                                                ),
                                                                 EntityTypes.PIG
                                                         )
                                         )
@@ -110,7 +115,7 @@ public class AstralCoresAdvancementProvider extends FabricAdvancementProvider {
                         )
                 )
                 .rewards(
-                        new net.minecraft.advancements.AdvancementRewards.Builder()
+                        new AdvancementRewards.Builder()
                                 .runs(
                                         Identifier.fromNamespaceAndPath(
                                                 AstralCores.MOD_ID,
@@ -125,5 +130,160 @@ public class AstralCoresAdvancementProvider extends FabricAdvancementProvider {
                                 "core/shadow_core"
                         )
                 );
+
+
+        Advancement.Builder.advancement()
+                .display(
+                        Blocks.OAK_LEAVES,
+                        Component.literal("Farming Dedication"),
+                        Component.literal(
+                                "Visit a forest, jungle, taiga, swamp, mangrove swamp and savanna, and obtain a Netherite Hoe with Mending and Unbreaking."
+                        ),
+                        null,
+                        AdvancementType.CHALLENGE,
+                        true,
+                        true,
+                        false
+                )
+
+                // Forest
+                .addCriterion(
+                        "forest",
+                        PlayerTrigger.TriggerInstance.located(
+                                LocationPredicate.Builder.location()
+                                        .setBiomes(
+                                                lookup.lookupOrThrow(Registries.BIOME)
+                                                        .getOrThrow(BiomeTags.IS_FOREST)
+                                        )
+                        )
+                )
+
+                // Jungle
+                .addCriterion(
+                        "jungle",
+                        PlayerTrigger.TriggerInstance.located(
+                                LocationPredicate.Builder.location()
+                                        .setBiomes(
+                                                lookup.lookupOrThrow(Registries.BIOME)
+                                                        .getOrThrow(BiomeTags.IS_JUNGLE)
+                                        )
+                        )
+                )
+
+                // Taiga
+                .addCriterion(
+                        "taiga",
+                        PlayerTrigger.TriggerInstance.located(
+                                LocationPredicate.Builder.location()
+                                        .setBiomes(
+                                                lookup.lookupOrThrow(Registries.BIOME)
+                                                        .getOrThrow(BiomeTags.IS_TAIGA)
+                                        )
+                        )
+                )
+
+                // Swamp
+                .addCriterion(
+                        "swamp",
+                        PlayerTrigger.TriggerInstance.located(
+                                LocationPredicate.Builder.location()
+                                        .setBiomes(
+                                                HolderSet.direct(
+                                                        lookup.lookupOrThrow(Registries.BIOME)
+                                                                .getOrThrow(Biomes.SWAMP)
+                                                )
+                                        )
+                        )
+                )
+
+                // Mangrove Swamp
+                .addCriterion(
+                        "mangrove_swamp",
+                        PlayerTrigger.TriggerInstance.located(
+                                LocationPredicate.Builder.location()
+                                        .setBiomes(
+                                                HolderSet.direct(
+                                                        lookup.lookupOrThrow(Registries.BIOME)
+                                                                .getOrThrow(Biomes.MANGROVE_SWAMP)
+                                                )
+                                        )
+                        )
+                )
+
+                // Savanna
+                .addCriterion(
+                        "savanna",
+                        PlayerTrigger.TriggerInstance.located(
+                                LocationPredicate.Builder.location()
+                                        .setBiomes(
+                                                lookup.lookupOrThrow(Registries.BIOME)
+                                                        .getOrThrow(BiomeTags.IS_SAVANNA)
+                                        )
+                        )
+                )
+
+                // Netherite Hoe with Mending + Unbreaking
+                .addCriterion(
+                        "enchanted_netherite_hoe",
+                        InventoryChangeTrigger.TriggerInstance.hasItems(
+                                ItemPredicate.Builder.item()
+                                        .of(
+                                                lookup.lookupOrThrow(Registries.ITEM),
+                                                Items.NETHERITE_HOE
+                                        )
+                                        .withComponents(
+                                                DataComponentMatchers.Builder
+                                                        .components()
+                                                        .partial(
+                                                                DataComponentPredicates.ENCHANTMENTS,
+                                                                EnchantmentsPredicate.enchantments(
+                                                                        List.of(
+                                                                                new EnchantmentPredicate(
+                                                                                        lookup.lookupOrThrow(
+                                                                                                Registries.ENCHANTMENT
+                                                                                        ).getOrThrow(
+                                                                                                Enchantments.MENDING
+                                                                                        ),
+                                                                                        MinMaxBounds.Ints.atLeast(1)
+                                                                                ),
+                                                                                new EnchantmentPredicate(
+                                                                                        lookup.lookupOrThrow(
+                                                                                                Registries.ENCHANTMENT
+                                                                                        ).getOrThrow(
+                                                                                                Enchantments.UNBREAKING
+                                                                                        ),
+                                                                                        MinMaxBounds.Ints.atLeast(1)
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                        .build()
+                                        )
+                        )
+                )
+
+                // ALL criteria are required
+                .requirements(
+                        AdvancementRequirements.Strategy.AND
+                )
+
+                // Reward
+                .rewards(
+                        new AdvancementRewards.Builder()
+                                .runs(
+                                        Identifier.fromNamespaceAndPath(
+                                                AstralCores.MOD_ID,
+                                                "cores/nature_core"
+                                        )
+                                )
+                )
+
+                .save(
+                        consumer,
+                        Identifier.fromNamespaceAndPath(
+                                AstralCores.MOD_ID,
+                                "core/nature_core"
+                        )
+                );
     }
-}
+    }
